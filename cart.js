da631c9836231c3324626151d064908ec302ea19
Sid <118056742+Sidgovatski@users.noreply.github.com>
2023-09-20 07:18:28 -0700

    c

--- cart.js
+++ cart.js
@@ -1,49 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
     const cartItems = document.getElementById("cart-items");
-
+  
     function updateCartDisplay() {
-        let cart = [];
-        const cartData = localStorage.getItem("cart");
-        if (cartData) {
-            cart = JSON.parse(cartData);
+      cartItems.innerHTML = "";
+  
+      const cartData = JSON.parse(localStorage.getItem("cart")) || [];
+  
+      const cartMap = new Map();
+  
+      cartData.forEach((item) => {
+        if (cartMap.has(item)) {
+          const count = cartMap.get(item);
+          cartMap.set(item, count + 1);
+        } else {
+          cartMap.set(item, 1);
         }
-
-        cartItems.innerHTML = "";
-
-        const cartMap = new Map();
-
-        cart.forEach((item) => {
-            if (cartMap.has(item)) {
-                const count = cartMap.get(item);
-                cartMap.set(item, count + 1);
-            } else {
-                cartMap.set(item, 1);
-            }
+      });
+  
+      cartMap.forEach((count, item) => {
+        const cartItem = document.createElement("li");
+        cartItem.textContent = `${item} (x${count})`;
+  
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-button");
+  
+        deleteButton.addEventListener("click", function () {
+          cartData.splice(cartData.indexOf(item), 1);
+          localStorage.setItem("cart", JSON.stringify(cartData));
+          updateCartDisplay();
         });
-
-        cartMap.forEach((count, item) => {
-            const cartItem = document.createElement("li");
-            cartItem.textContent = `${item} (x${count})`;
-
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Delete";
-            deleteButton.classList.add("delete-button");
-            deleteButton.dataset.itemName = item;
-
-            deleteButton.addEventListener("click", function () {
-                const itemName = this.dataset.itemName;
-                const itemIndex = cart.indexOf(itemName);
-                if (itemIndex !== -1) {
-                    cart.splice(itemIndex, 1);
-                    localStorage.setItem("cart", JSON.stringify(cart));
-                    updateCartDisplay();
-                }
-            });
-
-            cartItem.appendChild(deleteButton);
-            cartItems.appendChild(cartItem);
-        });
+  
+        cartItem.appendChild(deleteButton);
+        cartItems.appendChild(cartItem);
+      });
     }
-
+  
     updateCartDisplay();
-});
+  });
+  

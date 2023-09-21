080935f786d316748bef7d2e6f48a07cb0940e32
Sid <118056742+Sidgovatski@users.noreply.github.com>
2023-09-21 11:39:23 -0700

    CSS Cleaning

--- cart.js
+++ cart.js
@@ -1,18 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const cartItems = document.getElementById("cart-items");
-  
-    function updateCartDisplay() {
-        let cart = [];
-        const cartData = localStorage.getItem("cart");
-        if (cartData) {
-            cart = JSON.parse(cartData);
-        }
 
+    function updateCartDisplay() {
+        const cartItems = document.getElementById("cart-items");
         cartItems.innerHTML = "";
+
+        const cartData = JSON.parse(localStorage.getItem("cart")) || [];
 
         const cartMap = new Map();
 
-        cart.forEach((item) => {
+        cartData.forEach((item) => {
             if (cartMap.has(item)) {
                 const count = cartMap.get(item);
                 cartMap.set(item, count + 1);
@@ -25,59 +22,29 @@ document.addEventListener("DOMContentLoaded", function () {
             const cartItem = document.createElement("li");
             cartItem.innerHTML = `
                 <div class="cart-item">
-                    <span>${item} (x${count})</span>
+                    <span>${item} (x<span class="item-quantity">${count}</span>)</span>
+                    <button class="increment-button" data-item-name="${item}">+</button>
                     <button class="delete-button" data-item-name="${item}">Delete</button>
                 </div>
             `;
+
+            const deleteButton = cartItem.querySelector(".delete-button");
+            deleteButton.addEventListener("click", function () {
+                cartData.splice(cartData.indexOf(item), 1);
+                localStorage.setItem("cart", JSON.stringify(cartData));
+                updateCartDisplay();
+            });
+
+            const incrementButton = cartItem.querySelector(".increment-button");
+            incrementButton.addEventListener("click", function () {
+                cartData.push(item);
+                localStorage.setItem("cart", JSON.stringify(cartData));
+                updateCartDisplay();
+            });
+
             cartItems.appendChild(cartItem);
         });
-
-        const deleteButtons = document.querySelectorAll(".delete-button");
-        deleteButtons.forEach((button) => {
-            button.addEventListener("click", function () {
-                const itemName = button.getAttribute("data-item-name");
-                const itemIndex = cart.indexOf(itemName);
-                if (itemIndex !== -1) {
-                    cart.splice(itemIndex, 1);
-                    localStorage.setItem("cart", JSON.stringify(cart));
-                    updateCartDisplay();
-                }
-            });
-        });
-      cartItems.innerHTML = "";
-  
-      const cartData = JSON.parse(localStorage.getItem("cart")) || [];
-  
-      const cartMap = new Map();
-  
-      cartData.forEach((item) => {
-        if (cartMap.has(item)) {
-          const count = cartMap.get(item);
-          cartMap.set(item, count + 1);
-        } else {
-          cartMap.set(item, 1);
-        }
-      });
-  
-      cartMap.forEach((count, item) => {
-        const cartItem = document.createElement("li");
-        cartItem.textContent = `${item} (x${count})`;
-  
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.classList.add("delete-button");
-  
-        deleteButton.addEventListener("click", function () {
-          cartData.splice(cartData.indexOf(item), 1);
-          localStorage.setItem("cart", JSON.stringify(cartData));
-          updateCartDisplay();
-        });
-  
-        cartItem.appendChild(deleteButton);
-        cartItems.appendChild(cartItem);
-      });
     }
-  
+
     updateCartDisplay();
-  });
-  
+});
